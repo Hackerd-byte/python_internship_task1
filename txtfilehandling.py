25e83e32d1203f905b328a58082b2498ca3d5e21
Hariprasath <178020439+Hackerd-byte@users.noreply.github.com>
2025-06-19 16:58:20 +0530

    Update txtfilehandling.py

--- txtfilehandling.py
+++ txtfilehandling.py
@@ -1,5 +1,6 @@
 data="Hello World!,Welcome to Python Programming."
 file=open("sample.txt",'w')
+print("sample.txt file created successfully.")
 file.write(data)
 file.close()
 file=open("sample.txt",'r')
