0d081c7939026e87033ca1e048fc5cdc9508dec2
Hariprasath <178020439+Hackerd-byte@users.noreply.github.com>
2025-06-19 17:01:48 +0530

    Update csvfilehandling.py

--- csvfilehandling.py
+++ csvfilehandling.py
@@ -5,6 +5,12 @@ data=[
 ]
 fields=["Name","Branch","Rollno"]
 with open("sample.csv",mode='w',newline='') as file:
+    print("sample.csv file created suceessfully.")
     writer=csv.DictWriter(file,fieldnames=fields)
     writer.writeheader()
     writer.writerows(data)
+with open('your_file.csv', 'r', newline='') as file:
+    csv_reader = csv.reader(file)    
+    header = next(csv_reader, None)
+    for row in csv_reader:
+        print(row)
