9ca4c570925dd5fac3ac570e88ac7addd47f50d5
Hariprasath <178020439+Hackerd-byte@users.noreply.github.com>
2025-06-19 17:04:30 +0530

    Update jsonfilehanding.py

--- jsonfilehanding.py
+++ jsonfilehanding.py
@@ -6,7 +6,7 @@ with open("sample.json",'w',encoding="utf-8") as file:
         "Roll_no":1
     }
     write=json.dump(data,file,indent=4)
-    print(file.name,"writed sucessfully")
+    print(file.name,"created sucessfully.")
     file.close()
 with open("sample.json",'r',encoding='utf-8') as file:
     data=json.load(file)
